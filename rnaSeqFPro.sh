b700b75c247472cd2510f9659c19999c323a921d
Milos Pjanic <pjanic.milos@gmail.com>
2016-09-21 23:21:17 -0700

    Update rnaSeqFPro.sh

--- rnaSeqFPro.sh
+++ rnaSeqFPro.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-#fastqc quality control
+#fastqc quality control - requires fastqc installed and placed in PATH
 
 ls -1 *fastqc.gz > commands
 sed -i 's/^/fastqc /g' commands
 source commands
 
-#mapping
+#mapping with bwa mem - requires bwa mem installed and copied to PATH
+
 files=(*fastq.gz)
 for (( i=0; i<${#files[@]} ; i+=2 )) ; do
     echo "${files[i]}" "${files[i+1]} \n" >> commands.2
 done
 
-sed -i 's/^/bwa\ mem\ -t\ 64\ /g' commands.2
+sed  -i 's/^/bwa\ mem\ -t\ 64\ /g' commands.2
+
+
