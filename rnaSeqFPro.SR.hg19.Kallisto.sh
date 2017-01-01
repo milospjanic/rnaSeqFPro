be9913944e1a9f66309b9c6ba3f5af6a1e7c5006
Milos Pjanic <pjanic.milos@gmail.com>
2016-12-31 19:32:24 -0800

    Update rnaSeqFPro.SR.hg19.Kallisto.sh

--- rnaSeqFPro.SR.hg19.Kallisto.sh
+++ rnaSeqFPro.SR.hg19.Kallisto.sh
@@ -13,57 +13,110 @@ mkdir FastQC_OUTPUT
 mv *zip FastQC_OUTPUT
 mv *html FastQC_OUTPUT
 
-###mapping with Kallisto - requires Kallisto installed and copied to PATH
+###mapping with STAR - requires STAR installed and copied to PATH
 
 files=(*fastq.gz)
-for (( i=0; i<${#files[@]} ; i+=2 )) ; do
-    mkdir "${files[i]}.${files[i+1]}.Kallisto"    
+for (( i=0; i<${#files[@]} ; i+=1 )) ; do
+    mkdir "${files[i]}.STAR"    
 done 
-
-#create kallisto index before running: kallisto index -i GENCODE_transcripts gencode.v25lift37.annotation.gtf
-
-#pseudo-mapping with kallisto
 
 GenomeDir='~/reference_genomes/hg19/'
 GenomeFasta='~/reference_genomes/hg19/hg19.fa'
-Gencode='quant -i GENCODE_transcripts'
-Parameters='--single -l 200 -s 20'
+CommonPars='--runThreadN 64 --outSAMattributes All --genomeLoad NoSharedMemory'
 
 files=(*fastq.gz)
-for (( i=0; i<${#files[@]} ; i+=2 )) ; do
+for (( i=0; i<${#files[@]} ; i+=1 )) ; do
 
-echo $(pwd)/${files[i]} $(pwd)/${files[i+1]}
-Reads="$(pwd)/"${files[i]}" $(pwd)/"${files[i+1]}" "
+echo $(pwd)/${files[i]}
+Reads="$(pwd)/"${files[i]}" --readFilesCommand zcat"
 echo $Reads
 
-  cat >> commands.2.${files[i]}.${files[i+1]}.tmp <<EOL
+  cat >> commands.2.${files[i]}.tmp <<EOL
 #!/bin/bash
-    echo Proccessing `pwd`: ${files[i]} ${files[i+1]}
+    echo Proccessing `pwd`: ${files[i]}
 
     # enter the correct folder
-	cd ${files[i]}.${files[i+1]}.Kallisto
-    # run Kallisto
-        kallisto $Gencode -o ${files[i]}.${files[i+1]}.output $Parameters $Reads
+	cd ${files[i]}.STAR
+    # run 1st pass
+        mkdir Pass1
+        cd Pass1
+        STAR $CommonPars --genomeDir $GenomeDir --readFilesIn $Reads
         cd ..
-        echo FINISHED ${pwd}/${files[i]} ${pwd}/${files[i+1]}  
+    # make splice junctions database file out of SJ.out.tab, filter out non-canonical junctions
+        mkdir GenomeForPass2
+        cd GenomeForPass2
+        awk 'BEGIN {OFS="\t"; strChar[0]="."; strChar[1]="+"; strChar[2]="-";} {if(\$5>0){print \$1,\$2,\$3,strChar[\$4]}}' ../Pass1/SJ.out.tab > SJ.out.tab.Pass1.sjdb
+    # generate genome with junctions from the 1st pass
+        STAR --genomeDir ./ --runMode genomeGenerate --genomeFastaFiles $GenomeFasta --sjdbFileChrStartEnd SJ.out.tab.Pass1.sjdb --sjdbOverhang 100 --runThreadN 64
+        cd ..
+    # run 2nd pass with the new genome
+        mkdir Pass2
+        cd Pass2
+        STAR $CommonPars --genomeDir ../GenomeForPass2 --readFilesIn $Reads
+        echo FINISHED ${pwd}/${files[i]} 
+        cd ..
+        cd ..
+        
 EOL
   done
 
-
-for (( i=0; i<${#files[@]} ; i+=2 )) ; do
-    sed -i "2i\ Reads=\"`pwd`/${files[i]} `pwd`/${files[i+1]} \"" commands.2.${files[i]}.${files[i+1]}.tmp
+files=(*fastq.gz)
+for (( i=0; i<${#files[@]} ; i+=1 )) ; do
+    sed -i "8i\\\tcd ${files[i]}.STAR" commands.2.${files[i]}.tmp
 done
 
-for (( i=0; i<${#files[@]} ; i+=2 )) ; do
-    source commands.2.${files[i]}.${files[i+1]}.tmp
+for (( i=0; i<${#files[@]} ; i+=1 )) ; do
+    sed -i "2i\ Reads=\"`pwd`/${files[i]} --readFilesCommand zcat\"" commands.2.${files[i]}.tmp
 done
 
-# Find all files with abundance.tsv extension and cut the first and $2 column, save it as .cut file
+for (( i=0; i<${#files[@]} ; i+=1 )) ; do
+    source commands.2.${files[i]}.tmp
+done
 
-find -name '*abundance.tsv' | xargs -I % sh -c 'cut -f1,4 % | sed "s/.*|E/E/g" | sed "s/|.*|.*|.*|.*|//g" > %.cut1;'
+#subscrips
+
+files=(*fastq.gz)
+for (( i=0; i<${#files[@]} ; i+=1 )) ; do
+    echo "${files[i]}" >> commands.2
+done
+
+touch sam.tmp
+
+files=(*fastq.gz)
+for (( i=0; i<${#files[@]} ; i+=1 )) ; do
+    echo "${files[i]}.sam" >> sam.tmp     
+done
+
+awk 'FNR==NR{a[FNR]=$0;next}{ print $0,">",a[FNR]}' sam.tmp commands.2 > merge.tmp
+
+rm sam.tmp
+rm commands.2
+rm merge.tmp
+
+
+###counting with featureCounts
+
+#naming the output files by the second to last subfolder in path
+
+find . -type f -wholename "*Pass2*sam" -exec sh -c '
+    for f
+        do echo $f
+        fileName=$(basename $f);
+        filePath=$(dirname $f);
+        lastDir=$(basename $filePath);
+        prevDir=$(basename $(dirname $filePath));
+        echo $prevDir
+        echo $lastDir
+        echo proccessing  $fileName from $(pwd)/$lastDir into $prevDir.counts.txt;
+        featureCounts -a gencode.v25lift37.annotation.gtf -o $prevDir.counts.txt -T 64 -t exon -g gene_id $f
+    done' sh {} +
+    
+# Find all files with .counts.txt extension and cut the first and $2 column, save it as .cut file
+
+find -name '*.counts.txt' | xargs -I % sh -c 'cut -f 1,7 %  > %.cut1;'
 
 # remove header
-find -name '*abundance.tsv.cut1' | xargs -I % sh -c 'tail -n+2 % > %.cut2;'
+find -name '*.counts.txt.cut1' | xargs -I % sh -c 'tail -n+2 % > %.cut2;'
 
 # download fileMulti2TableMod1.awk
 
@@ -71,7 +124,7 @@ wget https://raw.githubusercontent.com/milospjanic/fileMulti2TableMod1/master/fi
 
 # Find .file.cut files and call fileMulti2TableMod1.awk script to create master table
 
-filescut=$(ls *.cut1.cut2) 
+filescut=$(ls *.counts.txt.cut1.cut2) 
 awk -f fileMulti2TableMod1.awk $(echo $filescut)> mastertable
 
 #clean up mastertable
@@ -157,7 +210,7 @@ rm id_merge.ncrna.txt
 rm script.r
 
 
-###create  R script
+###create  R script for RGSEPD
 
 touch script.R
 
@@ -230,5 +283,8 @@ chmod 775 script.deseq.R
 ./script.deseq.R
 rm script.deseq.R
 
+
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+
+
