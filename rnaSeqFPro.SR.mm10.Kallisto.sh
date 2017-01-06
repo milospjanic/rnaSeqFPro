8d64a4baaf75e395c0588e56f8db92516339a70d
Milos Pjanic <pjanic.milos@gmail.com>
2017-01-06 15:19:53 -0800

    Update rnaSeqFPro.SR.mm10.Kallisto.sh

--- rnaSeqFPro.SR.mm10.Kallisto.sh
+++ rnaSeqFPro.SR.mm10.Kallisto.sh
@@ -39,7 +39,7 @@ for (( i=0; i<${#files[@]} ; i+=1 )) ; do
 echo $(pwd)/${files[i]}
 
 Reads="$(pwd)/"${files[i]}" "
-Index='-i GENCODE_transcripts_mouse'
+Index='-i $(pwd)/GENCODE_transcripts_mouse'
 Parameters='--single -l 200 -s 20'
 
 echo $Reads
