5108465bd4b3d0b5cfd608d630d73cca2aabbcc8
Milos Pjanic <pjanic.milos@gmail.com>
2017-01-06 14:47:12 -0800

    Update rnaSeqFPro.PE.hg19.Kallisto.sh

--- rnaSeqFPro.PE.hg19.Kallisto.sh
+++ rnaSeqFPro.PE.hg19.Kallisto.sh
@@ -38,12 +38,13 @@ for (( i=0; i<${#files[@]} ; i+=2 )) ; do
 
 echo $(pwd)/${files[i]} $(pwd)/${files[i+1]}
 Reads="$(pwd)/"${files[i]}" $(pwd)/"${files[i+1]}" "
+Index='-i GENCODE_transcripts_human'
+Parameters='-l 200 -s 20'
 echo $Reads
 
   cat >> commands.2.${files[i]}.${files[i+1]}.tmp <<EOL
 #!/bin/bash
-    Index='-i GENCODE_transcripts_human'
-    Parameters='-l 200 -s 20'
+    
     echo Proccessing `pwd`: ${files[i]} ${files[i+1]}
     
     # enter the correct folder
