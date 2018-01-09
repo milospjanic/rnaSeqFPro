5246b19a197051e242a83d99f7a79183ccfb9a6f
Milos Pjanic <pjanic.milos@gmail.com>
2018-01-08 17:05:17 -0800

    Update rnaSeqFPro.PE.hg19.Kallisto.sh

--- rnaSeqFPro.PE.hg19.Kallisto.sh
+++ rnaSeqFPro.PE.hg19.Kallisto.sh
@@ -1,96 +1,136 @@
 #!/bin/bash
 
-FILE="./GTEx_Analysis_v6p_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct"
-FILE2="./GTEx_Data_V6_Annotations_SampleAttributesDS.txt"
-GENES="./genes.txt"
+SECONDS=0
 
-#check if GTEx file exists, if not, download 
+###fastqc quality control - requires fastqc installed and placed in PATH
+
+ls -1 *fastq.gz > commands.1
+sed -i 's/^/.\/FastQC\/fastqc /g' commands.1
+
+source commands.1
+
+mkdir FastQC_OUTPUT
+mv *zip FastQC_OUTPUT
+mv *html FastQC_OUTPUT
+
+###pseudomapping with Kallisto - requires Kallisto installed and copied to PATH
+
+files=(*fastq.gz)
+for (( i=0; i<${#files[@]} ; i+=2 )) ; do
+    mkdir "${files[i]}.${files[i+1]}.kallisto"    
+done 
+
+#check if Kallisto index is present, if not create one
+FILE=GENCODE_transcripts_mouse 
 
 if [ ! -f $FILE ]
 then
-wget https://www.dropbox.com/s/c47ywdnnbge0y4j/GTEx_Analysis_v6p_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct.gz
-gunzip GTEx_Analysis_v6p_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct.gz
+kallisto index -i GENCODE_transcripts_mouse gencode.vM11.transcripts.fa.gz
 fi
 
-#check if the Sample Attribution file exists if not
-#download GTEx Analysis V6p release, de-identified, open access version of the sample annotations, available in dbGaP.	
+#pseudo-mapping with kallisto
 
-if [ ! -f $FILE2 ]
-then
-wget https://www.dropbox.com/s/23c3zs9igediuy1/GTEx_Data_V6_Annotations_SampleAttributesDS.txt
-fi
+GenomeDir='~/reference_genomes/mm10/'
+GenomeFasta='~/reference_genomes/mm10/mm10.fa'
+
+files=(*fastq.gz)
+for (( i=0; i<${#files[@]} ; i+=2 )) ; do
+
+echo $(pwd)/${files[i]} $(pwd)/${files[i+1]}
+
+Reads="$(pwd)/"${files[i]}" $(pwd)/"${files[i+1]}" "
+Index="-i $(pwd)/GENCODE_transcripts_mouse"
+Parameters='-l 200 -s 20'
+WorkDir="$(pwd)"
+    
+echo $Reads
+
+  cat >> commands.2.${files[i]}.${files[i+1]}.tmp <<EOL
+#!/bin/bash
+    
+    echo Proccessing `pwd`: ${files[i]} ${files[i+1]}
+    
+    # enter the correct folder
+	cd ${files[i]}.${files[i+1]}.kallisto
+    # run Kallisto
+        kallisto quant $Index -o ${files[i]}.${files[i+1]}.output $Parameters $Reads
+        cd ..
+        echo FINISHED $WorkDir/${files[i]} ${pwd}/${files[i+1]}  
+EOL
+  done
 
 
-#grep your tissue of interest, need exact tissue name from the GTEx file
-par=$1 #pass the first argument to a variable so that it can be used in grep with double quotes, in order to use spaced argument 
-grep "$par" GTEx_Data_V6_Annotations_SampleAttributesDS.txt | cut -f1 > sample_IDs
+for (( i=0; i<${#files[@]} ; i+=2 )) ; do
+    sed -i "2i\ Reads=\"`pwd`/${files[i]} `pwd`/${files[i+1]} \"" commands.2.${files[i]}.${files[i+1]}.tmp
+done
 
-input="./sample_IDs"
+for (( i=0; i<${#files[@]} ; i+=2 )) ; do
+    source commands.2.${files[i]}.${files[i+1]}.tmp
+done
 
-#write an extraction script
-echo "
-awk -v COLT=\$1 '
-        NR==1 {
-                for (i=1; i<=NF; i++) {
-                        if (\$i==COLT) {
-                                title=i;
-                                print \$i;
-                        }
-                }
-        }
-        NR>1 {
-                if (i=title) {
-                        print \$i;
-                }
-        }
-' \$2
-" > extractor.sh
-chmod 755 extractor.sh
+# Find all files with abundance.tsv extension and cut the first and $2 column, save it as .cut file
 
-#create header
-cat GTEx_Analysis_v6p_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct | head -n3 | tail -n1 > header
+find -name '*abundance.tsv' | xargs -I % sh -c 'cut -f1,4 % | sed "s/^[^|]*|//g" | sed "s/|.*|.*|.*|.*|//g" > %.cut1;'
 
-#exctract data for each gene in genes.txt from the GTEx_Analysis_v6p_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct (all GTEX individuals) 
-while
-IFS= read -r line
-do 
-grep $line GTEx_Analysis_v6p_RNA-seq_RNA-SeQCv1.1.8_gene_rpkm.gct > $line.gene.rpkm.txt
-cat header $line.gene.rpkm.txt > $line.gene.rpkm.header.txt
-done < "$GENES"
+# remove header
+find -name '*abundance.tsv.cut1' | xargs -I % sh -c 'tail -n+2 % > %.cut2;'
 
-#list the file names created in previous step to a file
-ls -1 *gene.rpkm.header.txt > gene.file.names.txt
-input2="./gene.file.names.txt"
+#select the top GENCODE isoform according to estimated read counts
 
-#
-while 
-IFS= read -r line 
-do 
-	echo $line >$line.output
+find -name '*abundance.tsv.cut1.cut2' | xargs -I % sh -c 'awk '\''{for(i=1;i<=NF;i++) t+=$i; print t"\t"$0; t=0}'\'' % | sort -k2,2 -k1,1nr | awk '\''!a[$2]++'\'' | cut -f2- > %.cleaned;'
+find -name '*abundance.tsv.cut1.cut2.cleaned' | xargs -I % sh -c 'tabsep %;'
 
-        while 
-        IFS= read -r samples
-        
-        do
-        ./extractor.sh $samples $line | awk 'NR%2==0'
-        done < "$input" >> $line.output
-        
-done < "$input2"
+# download fileMulti2TableMod1.awk
 
-paste *output > results.txt
+wget https://raw.githubusercontent.com/milospjanic/fileMulti2TableMod1/master/fileMulti2TableMod1.awk
 
-sed -i 's/.gene.rpkm.header.txt//g' results.txt
+# Find .file.cut files and call fileMulti2TableMod1.awk script to create master table
+
+filescut=$(find -name *.cut1.cut2.cleaned | sort | tr '\n' ' ')  
+awk -f fileMulti2TableMod1.awk $(echo $filescut)> mastertable
+
+#clean up mastertable
+sed -e 's/\.[0-9]*//g' mastertable | sed 's/_[0-9]*//g' > mastertable.2 #remove decimal points, roundup
+sed -e 's/\t[0-9]*e-[0-9]*/\t0/g' mastertable.2 > mastertable.3 #substitute e-N numbers to 0
+awk  '/e[+-]/{for(i = 1; i <= NF; i++) {if ($i~/^[0-9]*e[+-][0-9]*$/) printf "%.0f""\t", $i; else printf $i"\t"} printf "\n";next} {printf $0"\n"}' mastertable.3 > mastertable.4
+mv mastertable.4 mastertable
+rm mastertable.2
+rm mastertable.3
+
+# add header to mastertable
+
+find -name *.cut1.cut2 | sort | sed 's/.fastq.gz.output.abundance.tsv.cut1.cut2//g' | sed -e 's/.*.fastq.gz.kallisto.//g' | tr '\n' ' ' > header
+awk '{$1=" "$1}1' header > header2
+cat header2 mastertable > mastertable.2
+
+mv mastertable.2 mastertable
+
+# remove .cut files
+find -name *.cut1 | xargs rm
+find -name *.cut1.cut2 | xargs rm
+
+
+# remove fileMulti2TableMod1.awk
+rm fileMulti2TableMod1.awk
+
+#remove header
+rm header
+rm header2
+
+
+###write R script for ID conversion, needs biomaRt
 
 echo "#!/usr/bin/Rscript
-library(reshape2)
-library (ggplot2)
+library(biomaRt)
+listMarts(host=\"grch37.ensembl.org\")
 
-p<-read.table(\"results.txt\", header=T)
-pmelt<-melt(p)
-pdf(\"output_gtexex.pdf\")
-p <- ggplot(pmelt, aes(x=variable, y=value)) +geom_violin(aes(fill=variable)) + geom_dotplot(binaxis='y', stackdir='center', dotsize=.5, binwidth=.7)+labs(title=\"Expression in GTEx samples in $par\", x=\"Genes\", y=\"RPKM expression level\") + theme(axis.text.x = element_text(size=16),axis.text.y = element_text(size=16), axis.title.x = element_text(size=18),axis.title.y = element_text(size=18), plot.title=element_text(size=16))
-p
-dev.off()
+ensembl = useMart(\"ENSEMBL_MART_ENSEMBL\",dataset=\"mmusculus_gene_ensembl\", host=\"grch37.ensembl.org\")
+
+id_merge_mrna = getBM(attributes=c(\"ensembl_gene_id\",\"refseq_mrna\"),mart=ensembl)
+write.table(id_merge_mrna, file=\"id_merge.mrna.txt\", sep = \"\t\", quote =F, col.names=F, row.names=F)
+
+id_merge_ncrna = getBM(attributes=c(\"ensembl_gene_id\",\"refseq_ncrna\"),mart=ensembl)
+write.table(id_merge_ncrna, file=\"id_merge.ncrna.txt\", sep = \"\t\", quote =F, col.names=F, row.names=F)
 
 " > script.r
 
@@ -98,3 +138,121 @@ dev.off()
 
 chmod 775 script.r
 ./script.r
+
+awk < id_merge.mrna.txt > id_merge.mrna.short.txt 'NF>1'
+awk < id_merge.ncrna.txt > id_merge.ncrna.short.txt 'NF>1'
+mv id_merge.mrna.short.txt id_merge.mrna.txt
+mv id_merge.ncrna.short.txt id_merge.ncrna.txt
+cat id_merge.mrna.txt id_merge.ncrna.txt > id_merge.txt
+
+tabsep id_merge.txt
+tabsep mastertable
+
+#Use awk to append gene names
+
+awk 'NR==FNR {h[$1] = $1; h2[$1] = $2; next} {if (h2[$1]) print h2[$1], $0}' id_merge.txt mastertable > mastertable.genename
+tabsep mastertable.genename
+cut -f1,3- mastertable.genename >mastertable.genename.2
+mv mastertable.genename.2 mastertable.genename
+
+#remove duplicated rows, in id_merge multiple ENSG can correspond to one NM id, thus creating 
+sort mastertable.genename | awk '!a[$1]++' > mastertable.genename.2
+mv mastertable.genename.2 mastertable.genename
+
+#append header
+head -n1 mastertable > header
+sed -i 's/.fastq.gz//g'  header
+sed -i 's/.STAR//g' header
+cat header mastertable.genename > mastertable.genename.2
+mv mastertable.genename.2 mastertable.genename
+rm header
+tabsep mastertable.genename
+
+#remove temporary files
+
+rm id_merge.mrna.txt
+rm id_merge.ncrna.txt
+#rm id_merge.txt
+rm script.r
+
+###cleaning
+
+grep -v Geneid mastertable.genename > mastertable.1
+mv mastertable.1 mastertable.genename
+tabsep mastertable.genename
+
+###create  R script
+
+touch script.R
+
+echo "#!/usr/bin/Rscript" > script.R
+echo "library(rgsepd)" >>script.R
+echo "data<-read.delim(\"mastertable.genename\", header=T, row.names = 1, check.names=F)" >>script.R
+echo "meta<-read.delim(\"meta.data\", header=T)" >>script.R
+
+echo "G <- GSEPD_INIT(Output_Folder=\"GSEPD OUTPUT\", finalCounts=data, sampleMeta=meta, COLORS=c(blue=\"#4DA3FF\",black=\"#000000\",gold=\"#FFFF4D\"))" >> script.R
+
+echo "G <- GSEPD_ChangeConditions( G, c(\"A\",\"B\"))" >>script.R
+echo "G <- GSEPD_Process( G )" >> script.R
+
+chmod 775 script.R
+./script.R
+rm script.R
+
+#create R script for DESeq
+
+touch script.deseq.R
+
+echo "#!/usr/bin/Rscript" > script.deseq.R
+echo "library(DESeq)" >> script.deseq.R
+echo "data<-read.delim(\"mastertable.genename\", header=T, row.names = 1, check.names=F)" >>script.deseq.R
+echo "meta<-read.delim(\"meta.data\", header=T)" >>script.deseq.R
+###
+echo "conds<-factor(meta\$Condition)
+sampleTable<-data.frame(sampleName=colnames(data), condition=conds)
+countsTable = data
+#rownames(countsTable) <- countsTable$Geneid
+#countsTable <- countsTable[,-1]
+cds <- newCountDataSet( countsTable, conds)
+cds <- estimateSizeFactors( cds )
+sizeFactors( cds )
+head(counts(cds))
+head(counts(cds,normalized=TRUE))
+cds = estimateDispersions( cds, method=\"blind\", sharingMode=\"fit-only\" )
+str( fitInfo(cds) )
+plotDispEsts( cds )
+
+res = nbinomTest( cds, \"A\", \"B\" )
+head(res)
+plotMA(res)
+hist(res\$pval, breaks=100, col=\"skyblue\", border=\"slateblue\", main=\"\")
+resSig = res[ res\$padj < 0.1, ]
+write.csv( res, file=\"Result_Table.csv\" )
+write.csv( resSig[ order( resSig\$foldChange, -resSig\$baseMean ), ] , file=\"DownReg_Result_Table.csv\" )
+write.csv( resSig[ order( -resSig\$foldChange, -resSig\$baseMean ), ], file=\"UpReg_Result_Table.csv\" )
+
+cdsBlind = estimateDispersions( cds, method=\"blind\" )
+vsd = varianceStabilizingTransformation( cdsBlind )
+library(\"RColorBrewer\")
+library(\"gplots\")
+
+select = order(rowMeans(counts(cds)), decreasing=TRUE)[1:250]
+hmcol = colorRampPalette(brewer.pal(9, \"GnBu\"))(100)
+heatmap.2(exprs(vsd)[select,], col = hmcol, trace=\"none\", margin=c(10, 6))
+
+select = order(rowMeans(counts(cds)), decreasing=TRUE)[1:500]
+heatmap.2(exprs(vsd)[select,], col = hmcol, trace=\"none\", margin=c(10, 6))
+
+select = order(rowMeans(counts(cds)), decreasing=TRUE)[1:1000]
+heatmap.2(exprs(vsd)[select,], col = hmcol, trace=\"none\", margin=c(10, 6))
+
+print(plotPCA(vsd, intgroup=c(\"condition\")))
+
+" >> script.deseq.R
+
+chmod 775 script.deseq.R
+./script.deseq.R
+rm script.deseq.R
+
+duration=$SECONDS
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
