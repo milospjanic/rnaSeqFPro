114bfa126d3e4f51b1bb58c85e48300cb80a1db6
Milos Pjanic <pjanic.milos@gmail.com>
2019-09-16 14:14:12 +0200

    Update rnaSeqFPro.SR.hg19.sh

--- rnaSeqFPro.SR.hg19.sh
+++ rnaSeqFPro.SR.hg19.sh
@@ -286,7 +286,7 @@ heatmap.2(exprs(vsd)[select,], col = hmcol, trace=\"none\", margin=c(10, 6))
 print(plotPCA(vsd, intgroup=c(\"condition\")))
 
 # Make a basic volcano plot
-with(res, plot(log2FoldChange, -log10(pval), pch=20, main="Volcano plot", xlim=c(min(res$log2FoldChange[is.finite(res$log2FoldChange)]),max(res$log2FoldChange[is.finite(res$log2FoldChange)])), ylim=c(0,max(-log10(res$pval)[is.finite(-log10(res$pval))]))))
+with(res, plot(log2FoldChange, -log10(pval), pch=20, main=\"Volcano plot\", xlim=c(min(res$log2FoldChange[is.finite(res$log2FoldChange)]),max(res$log2FoldChange[is.finite(res$log2FoldChange)])), ylim=c(0,max(-log10(res$pval)[is.finite(-log10(res$pval))]))))
 
 # Add colored points: red if padj<0.05, orange of log2FC>1, green if both
 with(subset(res, padj<.05 ), points(log2FoldChange, -log10(pval), pch=20, col=\"red\"))
@@ -355,7 +355,7 @@ resSig <- subset(resOrdered, padj < 0.1)
 write.csv(as.data.frame(resSig), file=\"condition_treated_signif.csv\")
 
 # Make a basic volcano plot
-with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main="Volcano plot", xlim=c(min(res$log2FoldChange[is.finite(res$log2FoldChange)]),max(res$log2FoldChange[is.finite(res$log2FoldChange)])), ylim=c(0,max(-log10(res$pvalue)[is.finite(-log10(res$pvalue))]))))
+with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main=\"Volcano plot\", xlim=c(min(res$log2FoldChange[is.finite(res$log2FoldChange)]),max(res$log2FoldChange[is.finite(res$log2FoldChange)])), ylim=c(0,max(-log10(res$pvalue)[is.finite(-log10(res$pvalue))]))))
 
 # Add colored points: red if padj<0.05, orange of log2FC>1, green if both
 with(subset(res, padj<.05 ), points(log2FoldChange, -log10(pvalue), pch=20, col=\"red\"))
